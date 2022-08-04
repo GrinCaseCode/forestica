c73c045d2225a40702e1b276808cf505ab965f82
nickolay.grin <grinmail84@gmail.com>
2022-08-04 17:19:02 +0300

    Add files via upload

--- js/common.js
+++ js/common.js
@@ -74,6 +74,31 @@ if ( $(this).scrollTop() > 0 && $btn_fixed.hasClass("default") ){
 }
 
 
+$('.mySwiper').each(function(){
+	var slides = $(this).find(".swiper-slide");
+	var fraction = $(this).find(".swiper-fraction");
+	var slideCount = slides.length;
+	fraction.html(`1 / ${slideCount}`);
+
+	var swiper = new Swiper(this, {
+		slidesPerView: 1,
+		pagination: {
+			el: (this, ".swiper-pagination"),
+			dynamicBullets: true,
+		},
+
+
+  // Navigation arrows
+  navigation: {
+  	nextEl: (this, ".swiper-button-next"),
+  	prevEl: (this, ".swiper-button-prev")
+  },
+  on: {
+  	slideChange: () => {
+  		fraction.html(`${swiper.realIndex + 1} / ${slideCount}`);
+  	} }
+  });
+});
 
 
 /*slider news*/
@@ -172,6 +197,35 @@ const swiper4 = new Swiper(".swiper-card", {
 		clickable: true,
 	},
 
+});
+
+
+const swiper6 = new Swiper(".swiper-images", {
+	slidesPerView: 1,
+	nested: true,
+	pagination: {
+		el: ".swiper-images .swiper-pagination",
+		dynamicBullets: true,
+		clickable: true,
+	},
+
+});
+
+$('.swiper-recomendation').each(function(){
+	var slides7 = $(this).find(".swiper-slide");
+	var swiper7 = new Swiper(this, {
+		slidesPerView: "auto",
+		breakpoints: {
+
+			480: {
+
+			}
+		},
+		navigation: {
+			nextEl: (this, ".swiper-button-next"),
+			prevEl: (this, ".swiper-button-prev")
+		},
+	});
 });
 
 	//плавный скролл
@@ -281,14 +335,48 @@ const swiper4 = new Swiper(".swiper-card", {
 
 	//слайдер
 
-	$('.slider').slick({
-		arrows: true,
-		dots: true,
-		infinite: true,
+	$('.slider-for').slick({
+		arrows: false,
+		dots: false,
+		infinite: false,
 		slidesToShow: 1,
 		slidesToScroll: 1,
+		asNavFor: '.slider-nav',
+		touchThreshold: 1000,
 		prevArrow: '<div class="slick-prev slick-arrow"><i class="fas fa-chevron-left"></i><div/>',
 		nextArrow: '<div class="slick-next slick-arrow"><i class="fas fa-chevron-right"></i><div/>',
+	});
+
+	$('.slider-nav').slick({
+		arrows: true,
+		dots: false,
+		vertical: true,
+		verticalSwiping: true,
+		infinite: false,
+		slidesToShow: 4,
+		slidesToScroll: 1,
+		asNavFor: '.slider-for',
+		touchThreshold: 1000,
+		focusOnSelect: true,
+		prevArrow: '<div class="slick-prev slick-arrow"><i class="fal fa-chevron-up"></i><div/>',
+		nextArrow: '<div class="slick-next slick-arrow"><i class="fal fa-chevron-down"></i><div/>',
+		responsive: [
+		{
+			breakpoint: 992,
+			settings: {
+				vertical: false,
+				verticalSwiping: false,
+			}
+		},
+		{
+			breakpoint: 480,
+			settings: {
+				slidesToShow: 3,
+				vertical: false,
+				verticalSwiping: false,
+			}
+		}
+		]
 	});
 
 	$(".input-phone").mask("+7 (999) 999-99-99");
@@ -334,9 +422,14 @@ const swiper4 = new Swiper(".swiper-card", {
 	});
 
 
-	$(".title-sidebar").click(function(e) {
+	$(".link-filter").click(function(e) {
 		e.preventDefault();
 		$(".sidebar-filter").slideToggle(200);
+	});
+
+	$(".subtitle-sidebar").click(function(e) {
+		e.preventDefault();
+		$(this).siblings(".item-sidebar__content").slideToggle(200);
 	});
 
 
@@ -388,6 +481,16 @@ const swiper4 = new Swiper(".swiper-card", {
 			})
 		}).on("blur", updateValues)
 	});
+
+	$('.tabs li a').click(function(event) {
+		event.preventDefault();
+		$(this).parent().parent().find("li").removeClass('active');
+		$(this).parent().addClass('active');
+		$(".tab-pane").fadeOut(0);
+		var selectTab = $(this).attr("href");
+		$(selectTab).fadeIn(200);
+	});
+
 	 // стайлер для select
 	 $('select').styler();
 
@@ -397,7 +500,37 @@ const swiper4 = new Swiper(".swiper-card", {
 	//<a class="fancybox" data-fancybox-group="group"><img src="image.jpg" /></a>
 	$(".fancybox").fancybox();
 
+	jQuery('.quantity').each(function() {
+		var spinner = jQuery(this),
+		input = spinner.find('input[type="number"]'),
+		btnUp = spinner.find('.quantity-up'),
+		btnDown = spinner.find('.quantity-down'),
+		min = input.attr('min'),
+		max = input.attr('max');
 
+		btnUp.click(function() {
+			var oldValue = parseFloat(input.val());
+			if (oldValue >= max) {
+				var newVal = oldValue;
+			} else {
+				var newVal = oldValue + 1;
+			}
+			spinner.find("input").val(newVal);
+			spinner.find("input").trigger("change");
+		});
+
+		btnDown.click(function() {
+			var oldValue = parseFloat(input.val());
+			if (oldValue <= min) {
+				var newVal = oldValue;
+			} else {
+				var newVal = oldValue - 1;
+			}
+			spinner.find("input").val(newVal);
+			spinner.find("input").trigger("change");
+		});
+
+	});
 	//Кнопка "Наверх"
 	//Документация:
 	//http://api.jquery.com/scrolltop/
@@ -418,27 +551,3 @@ const swiper4 = new Swiper(".swiper-card", {
 /*polifyl*/
 /*! npm.im/object-fit-images 3.2.4 */
 var objectFitImages=function(){"use strict";function t(t,e){return"data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='"+t+"' height='"+e+"'%3E%3C/svg%3E"}function e(t){if(t.srcset&&!p&&window.picturefill){var e=window.picturefill._;t[e.ns]&&t[e.ns].evaled||e.fillImg(t,{reselect:!0}),t[e.ns].curSrc||(t[e.ns].supported=!1,e.fillImg(t,{reselect:!0})),t.currentSrc=t[e.ns].curSrc||t.src}}function i(t){for(var e,i=getComputedStyle(t).fontFamily,r={};null!==(e=u.exec(i));)r[e[1]]=e[2];return r}function r(e,i,r){var n=t(i||1,r||0);b.call(e,"src")!==n&&h.call(e,"src",n)}function n(t,e){t.naturalWidth?e(t):setTimeout(n,100,t,e)}function c(t){var c=i(t),o=t[l];if(c["object-fit"]=c["object-fit"]||"fill",!o.img){if("fill"===c["object-fit"])return;if(!o.skipTest&&f&&!c["object-position"])return}if(!o.img){o.img=new Image(t.width,t.height),o.img.srcset=b.call(t,"data-ofi-srcset")||t.srcset,o.img.src=b.call(t,"data-ofi-src")||t.src,h.call(t,"data-ofi-src",t.src),t.srcset&&h.call(t,"data-ofi-srcset",t.srcset),r(t,t.naturalWidth||t.width,t.naturalHeight||t.height),t.srcset&&(t.srcset="");try{s(t)}catch(t){window.console&&console.warn("https://bit.ly/ofi-old-browser")}}e(o.img),t.style.backgroundImage='url("'+(o.img.currentSrc||o.img.src).replace(/"/g,'\\"')+'")',t.style.backgroundPosition=c["object-position"]||"center",t.style.backgroundRepeat="no-repeat",t.style.backgroundOrigin="content-box",/scale-down/.test(c["object-fit"])?n(o.img,function(){o.img.naturalWidth>t.width||o.img.naturalHeight>t.height?t.style.backgroundSize="contain":t.style.backgroundSize="auto"}):t.style.backgroundSize=c["object-fit"].replace("none","auto").replace("fill","100% 100%"),n(o.img,function(e){r(t,e.naturalWidth,e.naturalHeight)})}function s(t){var e={get:function(e){return t[l].img[e?e:"src"]},set:function(e,i){return t[l].img[i?i:"src"]=e,h.call(t,"data-ofi-"+i,e),c(t),e}};Object.defineProperty(t,"src",e),Object.defineProperty(t,"currentSrc",{get:function(){return e.get("currentSrc")}}),Object.defineProperty(t,"srcset",{get:function(){return e.get("srcset")},set:function(t){return e.set(t,"srcset")}})}function o(){function t(t,e){return t[l]&&t[l].img&&("src"===e||"srcset"===e)?t[l].img:t}d||(HTMLImageElement.prototype.getAttribute=function(e){return b.call(t(this,e),e)},HTMLImageElement.prototype.setAttribute=function(e,i){return h.call(t(this,e),e,String(i))})}function a(t,e){var i=!y&&!t;if(e=e||{},t=t||"img",d&&!e.skipTest||!m)return!1;"img"===t?t=document.getElementsByTagName("img"):"string"==typeof t?t=document.querySelectorAll(t):"length"in t||(t=[t]);for(var r=0;r<t.length;r++)t[r][l]=t[r][l]||{skipTest:e.skipTest},c(t[r]);i&&(document.body.addEventListener("load",function(t){"IMG"===t.target.tagName&&a(t.target,{skipTest:e.skipTest})},!0),y=!0,t="img"),e.watchMQ&&window.addEventListener("resize",a.bind(null,t,{skipTest:e.skipTest}))}var l="fregante:object-fit-images",u=/(object-fit|object-position)\s*:\s*([-.\w\s%]+)/g,g="undefined"==typeof Image?{style:{"object-position":1}}:new Image,f="object-fit"in g.style,d="object-position"in g.style,m="background-size"in g.style,p="string"==typeof g.currentSrc,b=g.getAttribute,h=g.setAttribute,y=!1;return a.supportsObjectFit=f,a.supportsObjectPosition=d,o(),a}();
-
-const fraction = document.getElementById("fraction");
-const slides = document.querySelectorAll(".mySwiper .swiper-slide");
-const slideCount = slides.length;
-fraction.textContent = `1 / ${slideCount}`;
-
-const swiper = new Swiper(".mySwiper", {
-  // If we need pagination
-  pagination: {
-  	el: ".mySwiper .swiper-pagination",
-  	dynamicBullets: true,
-  },
-
-
-  // Navigation arrows
-  navigation: {
-  	nextEl: ".mySwiper .swiper-button-next",
-  	prevEl: ".mySwiper .swiper-button-prev" },
-
-
-  	on: {
-  		slideChange: () => {
-  			fraction.textContent = `${swiper.realIndex + 1} / ${slideCount}`;
-  		} } });
